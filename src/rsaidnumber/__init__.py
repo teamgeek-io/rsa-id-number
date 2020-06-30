510a3acd414b0641b5237adce0caa471072970a3
Andrei Todorov <andrei@teamgeek.io>
2020-06-30 11:35:14 +0200

    Parse and validate ID number

--- src/rsaidnumber/__init__.py
+++ src/rsaidnumber/__init__.py
@@ -1,56 +1,120 @@
 import re
 import logging
 from datetime import datetime
-from enum import Enum
+from enum import Enum, auto
 
 __version__ = "0.0.1"
 
 logger = logging.getLogger(__name__)
 
 RSA_ID_LENGTH = 13
+GENDER_FEMALE_MIN = 0
+GENDER_FEMALE_MAX = 4999
+GENDER_MALE_MIN = 5000
+GENDER_MALE_MAX = 9999
+CITIZENSHIP_SA_CITIZEN = 0
+CITIZENSHIP_PERMANENT_RESIDENT = 1
 
 
 class Gender(Enum):
-    FEMALE = "4"
-    MALE = "5"
+    FEMALE = auto()
+    MALE = auto()
 
 
 class Citizenship(Enum):
-    CITIZEN = "0"
-    RESIDENT = "1"
+    SA_CITIZEN = auto()
+    PERMANENT_RESIDENT = auto()
 
 
-def validate(id_number: str):
-    """Validate `id_number` against the RSA ID number format.
+class IdNumber:
+    def __init__(self, value: str):
+        self.value = value
+        self.error = None
+        self.date_of_birth = None
+        self.gender = None
+        self.citizenship = None
+        self.parse()
+
+    def clean(self):
+        """Return the value without any whitespace."""
+        return re.sub(r"\s", "", self.value or "")
+
+    def parse(self):
+        """Parse the value and validate against the RSA ID number format."""
+        self.error = None
+        self.date_of_birth = None
+        self.gender = None
+        self.citizenship = None
+
+        value = self.clean()
+        if not value:
+            self.error = f"'{value}' is not a valid RSA ID number!"
+            return
+
+        if not value.isdigit():
+            self.error = f"'{value}' contains non-digit characters!"
+            return
+
+        if len(value) != RSA_ID_LENGTH:
+            self.error = f"'{value}' is not {RSA_ID_LENGTH} digits!"
+            return
+
+        year = value[0:2]
+        month = value[2:4]
+        day = value[4:6]
+        try:
+            self.date_of_birth = datetime.strptime(
+                f"{year}{month}{day}", "%y%m%d"
+            )
+        except ValueError:
+            self.error = f"'{value}' contains an invalid date of birth!"
+            logger.debug(self.error, exc_info=True)
+            return
+
+        gender = int(value[6:10])
+        if gender >= GENDER_FEMALE_MIN and gender <= GENDER_FEMALE_MAX:
+            self.gender = Gender.FEMALE
+        else:
+            self.gender = Gender.MALE
+
+        citizenship = int(value[10])
+        if citizenship == CITIZENSHIP_SA_CITIZEN:
+            self.citizenship = Citizenship.SA_CITIZEN
+        elif citizenship == CITIZENSHIP_PERMANENT_RESIDENT:
+            self.citizenship = Citizenship.PERMANENT_RESIDENT
+        else:
+            self.error = f"Invalid citizenship indicator: '{citizenship}'!"
+            return
+
+    @property
+    def valid(self) -> bool:
+        """Return True if there is not error, False otherwise."""
+        return not self.error
+
+    def __repr__(self):
+        return self.clean()
+
+
+def parse(value: str, raise_exc: bool = True) -> IdNumber:
+    """Parse `value` and validate against the RSA ID number format.
 
     Args:
-        id_number: ID number to validate.
+        value: ID number string to parse and validate.
+
+    Returns:
+        A new `IdNumber` instance.
 
     Raises:
-        ValueError: If the ID number is invalid.
+        ValueError: If the ID number is invalid and `raise_exc` is True.
 
     Examples:
         >>> import rsaidnumber
-        >>> rsaidnumber.validate('1234567890123')
+        >>> value = '1234567890123'
+        >>> id_number = rsaidnumber.parse(value)
 
     """
-    if not id_number:
-        raise ValueError(f"'{id_number}' is not a valid RSA ID number!")
-
-    id_number = re.sub(r"\s", "", id_number)
-
-    if not id_number.isdigit():
-        raise ValueError(f"'{id_number}' contains non-digit characters!")
-
-    if len(id_number) != RSA_ID_LENGTH:
-        raise ValueError(f"'{id_number}' is not {RSA_ID_LENGTH} digits!")
-
-    year = id_number[0:2]
-    month = id_number[2:4]
-    day = id_number[4:6]
-    try:
-        datetime.strptime(f"{month}{year}{day}", "%y%m%d")
-    except ValueError:
-        msg = f"'{id_number}' contains an invalid date of birth!"
-        logger.exception(msg, id_number)
-        raise ValueError(msg)
+    id_number = IdNumber(value)
+    id_number.parse()
+    if not id_number.valid and raise_exc:
+        raise ValueError(id_number.error)
+    return id_number
